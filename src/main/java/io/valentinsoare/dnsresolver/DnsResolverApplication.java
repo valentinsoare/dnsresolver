bcb3354fda8021f744d07919e13569862e006672
valentinsoare <soarevalentinn@gmail.com>
2024-06-01 06:55:05 +0300

    init phase

---
+++ src/main/java/io/valentinsoare/dnsresolver/DnsResolverApplication.java
@@ -0,0 +1,13 @@
+package io.valentinsoare.dnsresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnsResolverApplication.class, args);
+    }
+
+}
