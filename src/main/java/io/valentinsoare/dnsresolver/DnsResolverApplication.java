1e34524ec3fb01e22256e83db7c3f83e078ff0ce
valentinsoare <soarevalentinn@gmail.com>
2024-06-25 17:55:10 +0300

    README.md init

--- src/main/java/io/valentinsoare/dnsresolver/DnsResolverApplication.java
+++ src/main/java/io/valentinsoare/dnsresolver/DnsResolverApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DnsResolverApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DnsResolverApplication.class, args);
     }
-
 }
+
+
